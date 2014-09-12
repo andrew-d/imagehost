60e49aacd1668d2534fe89e5fb81bb5bf421927f
Andrew Dunham <andrew@du.nham.ca>
2014-09-11 23:53:06 -0700

    Remove dead code

--- util.go
+++ util.go
@@ -2,41 +2,8 @@ package main
 
 import (
 	"crypto/rand"
-	"encoding/json"
-	"net/http"
 	"io"
 )
-
-type errorInfo struct {
-	err  error
-	msg  string
-	code int
-}
-
-func printError(w http.ResponseWriter, i errorInfo) {
-	js := map[string]interface{}{
-		"status": "error",
-	}
-	if len(i.msg) > 0 {
-		js["msg"] = i.msg
-	}
-	if i.err != nil {
-		js["error"] = i.err.Error()
-	}
-
-	if i.code == 0 {
-		i.code = 500
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(i.code)
-	json.NewEncoder(w).Encode(js)
-}
-
-func printJson(w http.ResponseWriter, js map[string]interface{}) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(js)
-}
 
 // Note: slightly biased towards first 8 characters of the alphabet, since 255
 // isn't a multiple of 62 (length of alphanum).  We don't really care that
